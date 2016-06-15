858fbf55fe1973f32c6220793b48eafbb63c2b2a
Rohan0401 <rohan.rajput4@gmail.com>
2016-06-15 16:54:57 -0400

    My first commit

---
+++ app/setup.js
@@ -0,0 +1,6 @@
+window._ = require('underscore'); // Backbone can't see it otherwise
+
+var Backbone = require('backbone');
+Backbone.$ = window.$; // Use the jQuery from the script tag
+Backbone.Marionette = require('backbone.marionette');
+
